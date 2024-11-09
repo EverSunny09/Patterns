3453917c5439f9a27a68e0dc2b08258ee6215a5f
Sunny George <Sunny.George@stepstone.com>
2024-11-09 16:38:18 +0000

    refactored

--- Program.cs
+++ Program.cs
@@ -217,49 +217,6 @@ for (int i = 0; i < n - 1; i++)
 #endregion
 
 #region Butterfly Pattern
-
-/*int n = 7;
-
-for (int i = 0; i < n; i++)
-{
-    for (int j = 0; j < i+1; j++)
-    {
-        Console.Write("*");
-    }
-
-    for (int j = 0; j < ((2 * n - 2) - 2 * i); j++)
-    {
-        Console.Write(" ");
-    }
-    for (int j = 0; j < i + 1; j++)
-    {
-        Console.Write("*");
-    }
-    Console.WriteLine();
-}
-
-for (int i = 0; i < n; i++)
-{
-    for (int j = i; j < n; j++)
-    {
-        Console.Write("*");
-    }
-    for (int j = 0; j < i; j++)
-    {
-        Console.Write(" ");
-    }
-    for (int j = 0; j < i; j++)
-    {
-        Console.Write(" ");
-    }
-    for (int j = i; j < n; j++)
-    {
-        Console.Write("*");
-    }
-    
-    Console.WriteLine();
-}*/
-
 /*int n = 5;
 
 for (int i = 0; i < n; i++)
